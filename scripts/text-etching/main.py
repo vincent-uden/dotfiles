4746d8733e4063cc28e2cd2b4a0e650ee9f53d28
Vincent Udén <vincentuden@gmail.com>
2018-12-15 12:49:52 +0100

    Changed text etcher

--- scripts/text-etching/main.py
+++ scripts/text-etching/main.py
@@ -3,9 +3,17 @@ from subprocess import call
 from etch import etch_text
 from tasklist import get_tasks
 from settings import *
+from math import ceil
 
 def etch_wallpaper():
-    etch_text("- TODO -\n" + get_tasks(TASK_FILE, as_text=True))
+    all_tasks = get_tasks(TASK_FILE, as_text=False)
+    longest = max(all_tasks, key=len)
+    longest_half_len = ceil(float(len(longest))/2)
+    print(len(longest))
+    print(longest_half_len)
+    todo_line = "-" * (longest_half_len - 4) + "- TODO -" + "-" * (longest_half_len - 4) + "\n"
+    print(len(todo_line))
+    etch_text(todo_line + get_tasks(TASK_FILE, as_text=True) + "-" * (len(todo_line) - 1))
     cwd = path.dirname(path.abspath(__file__))
     system(FEH_PATH + " --bg-scale " + cwd + "/etched-wp.png")
 
