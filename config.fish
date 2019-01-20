4b427840103010661785a5c6ddc4ed7ef7f6e3b4
Vincent Udén <vincentuden@gmail.com>
2019-01-20 18:39:59 +0100

    Added ruby path

--- config.fish
+++ config.fish
@@ -4,3 +4,5 @@ alias ls="ls --group-directories-first --color -F -h -x"
 alias chrome="google-chrome-stable"
 alias linecount="~/Github/code-line-counter/line_counter"
 alias bg="python3 ~/Github/dotfiles/scripts/text-etching/tasklist.py"
+
+set -gx PATH ~/.gem/ruby/2.6.0/bin $PATH
